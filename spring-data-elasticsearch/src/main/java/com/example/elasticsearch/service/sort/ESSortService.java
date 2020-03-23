5aa3b2366e2409dc96cb33957c873b364a8359f0
song.cao <song.cao@zhangmen.com>
2020-03-23 23:12:05 +0800

    spring-boot-es

---
+++ spring-data-elasticsearch/src/main/java/com/example/elasticsearch/service/sort/ESSortService.java
@@ -0,0 +1,17 @@
+package com.example.elasticsearch.service.sort;
+
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+
+/**
+ * es 排序服务
+ *
+ * @author song.cao
+ * @since 2020/3/23 22:52
+ */
+public class ESSortService {
+
+    public void sort1() {
+        NativeSearchQuery nativeSearchQuery = new NativeSearchQuery();
+
+    }
+}
